5fe63026cb507a57e58d143666146b251d902f2c
mekaleka <www.no-reply.com>
2020-03-29 18:32:42 -0700

    updated response in serviceworker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -32,3 +32,14 @@ self.addEventListener("fetch", function(event){
         )
     }
 })
+
+event.respondWith(
+    fetch(event.request).catch(function(){
+        return caches.match(event.request).then(function(response){
+            if (response) return response
+            else if(event.request.headers.get("accept").includes("text/html")){
+                return caches.match("/")
+            }
+        })
+    })
+)
